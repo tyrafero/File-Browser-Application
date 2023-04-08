4a662ff9695821a4768ff6f230a56fa02b27a1fe
tyrafero <50377507+tyrafero@users.noreply.github.com>
2023-04-08 22:30:22 +0545

    Delete del_folder.py

--- del_folder.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def rm_folder(path):
-    try:
-        os.rmdir(path)
-    except OSError as e:
-        print(f"You got {e.filename}-{e.strerror} error")
-
-rm_folder(input("Enter the file path: "))
